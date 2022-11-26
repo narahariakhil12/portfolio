3538150cec7bee87031d3c4c39aec80801edf900
narahariakhil12 <narahariakhil1@gmail.com>
2022-11-26 23:28:27 +0100

    initial

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='w-full text-center bg-[#0a192f] text-gray-300'> <small>&copy; Copyright 2023, with love Akhil Narahari</small> </footer> 
+  )
+}
---
+++ src/data/data.js
@@ -0,0 +1,37 @@
+import WorkIm from '../assets/projects/workImg.jpeg'
+import RealEst from '../assets/projects/realestate.jpg'
+import appointment from '../assets/projects/appointment.jpeg'
+import quiz from '../assets/projects/quiz.jpg'
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:appointment,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:quiz,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:WorkIm,
+        github:"",
+        live:"",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+
+
+]
